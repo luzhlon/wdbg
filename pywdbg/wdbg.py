50d8bd10cac6ae95f1b225b48fdffa614d196c37
luzhlon <luzhlon@outlook.com>
2017-08-08 17:40:43 +0800

    [Enhancement] startup the wdbg.exe from local is possible

--- pywdbg/wdbg.py
+++ pywdbg/wdbg.py
@@ -2,28 +2,22 @@
 import time, os
 import srpc
 from K import ERROR, ENGOPT, EVENT, STATUS
-from threading import Thread, Condition, Lock
+from threading import Thread, Event
 
-PATH = __file__
 # Breakpoint callbacks
 bp_callback = {}
-# WDbg object
-dbg = None
-# Custom command
-class cmd:
-    pass
 
 # Auxiliary session's handler
 class AuxHandler:
     def heartbeat(ss, n):
         ss.notify('heartbeat', n)
 
-inp = input
+INPUT = input
 # Main session's handler
 class MainHandler:
     def input(ss, data):
-        global inp
-        return inp()
+        global INPUT
+        return INPUT()
 
     def output(ss, data):
         print(data.decode('gbk'), end = '')
@@ -44,16 +38,14 @@ backfset = set(['stepinto', 'stepover', 'run', 'waitevent', 'exec'])
 
 class WDbg:
     def __init__(self):
-        self._taskcond = Condition()    # condition for waitting task
-        self._compcond = Condition()    # condition for waitting to complete task
-        self._compcond.acquire()
+        self._taskcond = Event()    # condition for waitting task
+        self._compcond = Event()    # condition for waitting to complete task
         def backthread():
-            self._taskcond.acquire()
-            c = self._compcond
             while True:
+                self._taskcond.clear()
                 self._taskcond.wait()
                 self._result = self._fun(*self._args)
-                c.acquire(); c.notify(); c.release()
+                self._compcond.set()
         self._back = Thread(target = backthread)
         self._back.start()
 
@@ -76,8 +68,8 @@ class WDbg:
         return f
 
     def _backrun(self):
-        c = self._taskcond
-        c.acquire(); c.notify(); c.release()
+        self._compcond.clear()
+        self._taskcond.set()
         while True:
             try:
                 if self._compcond.wait(0.2):
@@ -119,37 +111,66 @@ class WDbg:
             except KeyboardInterrupt:
                 break
 
+PATH = __file__
+
 def search_wdbg(arch):
+    '''search the path of the wdbg.exe'''
+
     global PATH
     assert arch == 'x86' or arch == 'x64'
 
     curpath = os.path.abspath(PATH)
     upath = os.path.split(os.path.dirname(curpath))[0]
-    path = [upath + x + arch + '/wdbg.exe'
-            for x in ['/build/debug/', '/bin/']]
+    path = [upath + x + arch + '/wdbg.exe' for x in ['/build/debug/', '/bin/']]
 
     for p in path:
         if os.path.exists(p):
             return p
 
-def start(arch = None):
-    '''start the wdbg subprocess'''
-    import subprocess, platform
-    if not arch:
-        arch = 'x64' if platform.architecture()[0] == '64bit' else 'x86'
-    wdbg = subprocess.Popen([search_wdbg(arch), '-D'], stdin = subprocess.PIPE)
+def start_local(arch = 'x86'):
+    '''start the wdbg subprocess, and return its listened port'''
+
+    path = search_wdbg(arch)
+    if not path:
+        return None
+
+    from subprocess import STARTUPINFO, Popen, SW_HIDE
+    from subprocess import CREATE_NEW_CONSOLE, STARTF_USESHOWWINDOW, PIPE
+
+    si = STARTUPINFO()
+    si.dwFlags = STARTF_USESHOWWINDOW
+    si.wShowWindow = SW_HIDE
+    wdbg = Popen([path, '-D'], bufsize = 0, startupinfo = si,
+            creationflags = CREATE_NEW_CONSOLE, stdin = PIPE, stdout = PIPE)
+    import re
+    line = wdbg.stdout.readline().decode()
+    port = re.search(r'\d+', line)
+    if port:
+        return int(port.group(0))
+
+# WDbg object
+_wdbg = None
 
 def connect(addr = None, port = None):
-    global dbg
-    if dbg: return None
-    dbg = WDbg()
-    dbg._mss = srpc.Client()
-    dbg._mss.handler = MainHandler
-    dbg._mss.connect(addr, port)
+    global _wdbg
 
-    dbg._ass = srpc.Client()
-    dbg._ass.handler = AuxHandler
-    dbg._ass.connect(addr, port)
-    Thread(target = dbg._ass.run).start()
+    if not _wdbg:
+        _wdbg = WDbg()
+        _wdbg._mss = srpc.Client()
+        _wdbg._mss.handler = MainHandler
+        _wdbg._mss.connect(addr, port)
 
-    return dbg
+        _wdbg._ass = srpc.Client()
+        _wdbg._ass.handler = AuxHandler
+        _wdbg._ass.connect(addr, port)
+        Thread(target = _wdbg._ass.run).start()
+
+        return _wdbg
+
+def startup(arch = 'x86'):
+    '''startup the wdbg subprocess and connect it, return the WDbg object'''
+
+    port = start_local(arch)
+    if port:
+        return connect('127.0.0.1', port)
+
